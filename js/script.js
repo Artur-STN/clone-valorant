23b5ea38b5baa95b1af781afd355d81e06eb034b
Artur Santos <artur_teixeira10@hotmail.com>
2021-04-03 12:45:57 -0300

    code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+var counter = 'claro'
+
 function insertFundo() {
 
-    document.getElementById('container').style.background = 'url(../imagens/background-claro.jpg)  no-repeat'
+    if (counter == 'escuro') {
+
+        document.getElementById('container').style.background = 'url(../imagens/background-escuro.jpg)  no-repeat'
+        counter = 'claro'
+        
+    } else if (counter == 'claro') {
+
+        document.getElementById('container').style.background = 'url(../imagens/background-claro.jpg)  no-repeat'
+        counter = 'escuro'
+
+        
+    }
+
     document.getElementById('container').style.backgroundSize = '100%'
 
 }
